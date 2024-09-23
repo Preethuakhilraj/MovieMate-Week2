6dc4e0e77e82fcfd7a0b7c2397abe12dad6c2f76
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-09-24 04:03:23 +0530

    node modules removed

--- Frontend/src/Pages/axiosinterceptor.js
+++ Frontend/src/Pages/axiosinterceptor.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance=axios.create({
-baseURL:'http://localhost:3000'
+// baseURL:'http://localhost:3000'
+baseURL: 'https://moviemate-server.vercel.app/'
 })
 axiosInstance.interceptors.request.use((config)=>{
 const accessToken=sessionStorage.getItem('token')
